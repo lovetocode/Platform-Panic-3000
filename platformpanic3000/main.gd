756be986052a47c8bae6d1c907f59809f92c3d4b
lovetocode <dshaggy28@yahoo.com>
2024-12-27 17:33:01 -0800

    Added game assets

---
+++ platformpanic3000/main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/wrold.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
